c7c49b27d7946e9d8481ade6e613cd4437e1de19
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-22 15:12:37 -0400

    commit tree 49ef02cf1611794f4a5d9e09c49ed409ab44add9

---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  attr_accessor :name
+
+  def initialize(word) #can use (name); it can be called whatever you like! 
+    @name = word #needs to the same as the argument being passed in! 
+  end
+
+end
